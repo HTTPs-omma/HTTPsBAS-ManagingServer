99ae3ba116ba8b3febb48df16d990f09974bb32e
CherryPichu <uskawjdu@gmail.com>
2024-08-25 17:53:23 +0900

    git commit -m "HTTPS-53 <중간 작업 >"

---
+++ Core/AgentManager.go
@@ -0,0 +1,34 @@
+package Core
+
+/*
+	Agent Model = Managing + model
+
+*/
+
+type AgentManager struct {
+}
+
+func (self *AgentManager) createAgent(uuid string) error {
+
+	return nil
+}
+
+/*
+agent 의 상태 Status 는 총 3가지로 나뉜다.
+
+Running : 동작중인 상태
+waiting : 대기중인 상태
+Stopping : 정지후 사라지기 전에 상태
+*/
+func (self *AgentManager) checkAgent(uuid string) bool {
+
+	return false
+}
+
+func (self *AgentManager) updateAgent(uuid string) bool {
+
+}
+
+func (self *AgentManager) deleteAgent(uuid string) bool {
+
+}
--- app.go
+++ app.go
@@ -1,1 +1,20 @@
-package Managed_Server
+package main
+
+import (
+	"github.com/gofiber/fiber/v3"
+	"log"
+)
+
+func main() {
+	// Initialize a new Fiber app
+	app := fiber.New()
+
+	// Define a route for the GET method on the root path '/'
+	app.Get("/", func(c fiber.Ctx) error {
+		// Send a string response to the client
+		return c.SendString("Hello, World 👋!")
+	})
+
+	// Start the server on port 3000
+	log.Fatal(app.Listen(":3000"))
+}
