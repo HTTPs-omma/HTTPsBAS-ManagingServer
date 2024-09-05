e4cdbcb2e24914e3a078ba8e88702948817ced1a
CherryPichu <uskawjdu@gmail.com>
2024-09-05 21:57:44 +0900

    HTTPS-53 < JobManager 구현 >

---
+++ Core/JobManager.go
@@ -0,0 +1,47 @@
+package Core
+
+import (
+	"time"
+)
+
+type JobData struct {
+	TechnicalID string `json:"technicalID"`
+	AgentUUID   string `json:"agentUUID"`
+	MessageUUID string `json:"messageUUID"`
+	createAt    time.Time
+}
+
+type JobManager struct {
+	dataMap map[string][]JobData
+}
+
+func NewJobManager() *JobManager {
+	return &JobManager{
+		dataMap: make(map[string][]JobData),
+	}
+}
+
+func popFront(slice []JobData) (JobData, []JobData) {
+	if len(slice) > 0 {
+		job := slice[0]
+		return job, slice[1:]
+	}
+	return slice[0], slice
+}
+
+func (jm *JobManager) GetData(agentUUID string) (*JobData, bool) {
+	jobs, exists := jm.dataMap[agentUUID]
+
+	if len(jobs) > 0 {
+		var job JobData
+		job, jm.dataMap[agentUUID] = popFront(jobs)
+		return &job, exists
+	}
+
+	return &JobData{}, exists
+}
+
+func (jm *JobManager) insertData(jobData JobData) bool {
+	jm.dataMap[jobData.AgentUUID] = append(jm.dataMap[jobData.AgentUUID], jobData)
+	return true
+}
