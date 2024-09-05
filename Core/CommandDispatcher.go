82a98408aa45039ddc4bbf8a3d73c3e6521cbeb7
CherryPichu <uskawjdu@gmail.com>
2024-09-05 21:51:49 +0900

    HTTPS-53 < JobManager 구현 >

--- Core/CommandDispatcher.go
+++ Core/CommandDispatcher.go
@@ -5,32 +5,28 @@ import (
 	"github.com/HTTPs-omma/HSProtocol/HSProtocol"
 	"github.com/your/repo/Model"
 )
+
 // https://github.com/HTTPs-omma/HSProtocol
 type CommandDispatcher struct {
-
 }
 
-
-
-
-
-func (cd *CommandDispatcher)action(hs *HSProtocol.HS) (*HSProtocol.HS, error) {
+func (cd *CommandDispatcher) action(hs *HSProtocol.HS) (*HSProtocol.HS, error) {
 	//hsMgr := HSProtocol.NewHSProtocolManager()
 
 	switch hs.Command {
-	case 0b0000000001 :
+	case 0b0000000001:
 		return updateHealth(hs)
-	case 0b0000000010 :
+	case 0b0000000010:
 		return updateProtocol(hs)
-	case 0b0000000011 :
+	case 0b0000000011:
 		return postSystemInfo(hs)
-	case 0b0000000100 :
-		break; // 예약
-	case 0b0000000101 :
+	case 0b0000000100:
+		break // 예약
+	case 0b0000000101:
 		return postApplicationInfo(hs)
-	case 0b0000000110 :
+	case 0b0000000110:
 		return getProcedure(hs)
-	case 0b0000000111 :
+	case 0b0000000111:
 		return postLogOfProcedure(hs)
 	}
 
@@ -38,17 +34,15 @@ func (cd *CommandDispatcher)action(hs *HSProtocol.HS) (*HSProtocol.HS, error) {
 }
 
 // Command: 1 (0b0000000001)
-func updateHealth(hs *HSProtocol.HS)(*HSProtocol.HS, error){
+func updateHealth(hs *HSProtocol.HS) (*HSProtocol.HS, error) {
 	agsmd := Model.NewAgentStatusDB()
 	rst, err := agsmd.ExistRecord()
 	if err != nil {
 		return nil, err
 	}
-	if( rst ) {
+	if rst {
 		return nil, fmt.Errorf("Agent Status DB : no Records")
 	}
-
-
 
 	records, err := agsmd.SelectRecords()
 	if err != nil {
@@ -57,9 +51,8 @@ func updateHealth(hs *HSProtocol.HS)(*HSProtocol.HS, error){
 
 	hs_uuid := string(hs.UUID[:])
 
-
 	flag := false
-	for _, record  := range records {
+	for _, record := range records {
 		if record.UUID == hs_uuid {
 			flag = true
 		}
@@ -67,41 +60,40 @@ func updateHealth(hs *HSProtocol.HS)(*HSProtocol.HS, error){
 
 	if flag == true {
 		agsmd.UpdateRecord(&Model.AgentStatusRecord{
-			UUID : string(hs.UUID[:]),
+			UUID:   string(hs.UUID[:]),
 			Status: Model.BinaryToAgentStatus(hs.HealthStatus),
 		})
 		return &HSProtocol.HS{ // ACK
-			ProtocolID: hs.ProtocolID,
-			Command: 0b0000000000,
-			UUID: hs.UUID,
-			HealthStatus: hs.HealthStatus,
+			ProtocolID:     hs.ProtocolID,
+			Command:        0b0000000000,
+			UUID:           hs.UUID,
+			HealthStatus:   hs.HealthStatus,
 			Identification: hs.Identification,
-			TotalLength: hs.TotalLength,
-			Data: []byte{},
+			TotalLength:    hs.TotalLength,
+			Data:           []byte{},
 		}, nil
-	} else if (flag == false) && ( hs.HealthStatus == uint8(Model.Waiting) )  {
+	} else if (flag == false) && (hs.HealthStatus == uint8(Model.Waiting)) {
 		agsmd.InsertRecord(&Model.AgentStatusRecord{
-			UUID : string(hs.UUID[:]),
+			UUID:   string(hs.UUID[:]),
 			Status: Model.BinaryToAgentStatus(hs.HealthStatus),
 		})
 
 		return &HSProtocol.HS{ // ACK
-			ProtocolID : hs.ProtocolID,
-			Command: 0b0000000000,
-			UUID: hs.UUID,
-			HealthStatus: hs.HealthStatus,
+			ProtocolID:     hs.ProtocolID,
+			Command:        0b0000000000,
+			UUID:           hs.UUID,
+			HealthStatus:   hs.HealthStatus,
 			Identification: hs.Identification,
-			TotalLength: hs.TotalLength,
-			Data: []byte{},
+			TotalLength:    hs.TotalLength,
+			Data:           []byte{},
 		}, nil
 	}
-
 
 	return nil, fmt.Errorf("incorrect AgentStatusRecords")
 }
 
 // Command: 2 (0b0000000010)
-func updateProtocol(hs *HSProtocol.HS)(*HSProtocol.HS, error){
+func updateProtocol(hs *HSProtocol.HS) (*HSProtocol.HS, error) {
 
 	// protocolID := binary.BigEndian.Uint32(hs.Data)
 	agsmd := Model.NewAgentStatusDB()
@@ -109,10 +101,9 @@ func updateProtocol(hs *HSProtocol.HS)(*HSProtocol.HS, error){
 	if err != nil {
 		return nil, err
 	}
-	if( rst ) {
+	if rst {
 		return nil, fmt.Errorf("Agent Status DB : no Records")
 	}
-
 
 	records, err := agsmd.SelectRecords()
 	if err != nil {
@@ -120,9 +111,9 @@ func updateProtocol(hs *HSProtocol.HS)(*HSProtocol.HS, error){
 	}
 	hs_uuid := string(hs.UUID[:])
 
-	for _, record  := range records {
+	for _, record := range records {
 		if record.UUID == hs_uuid {
-			record.Protocol = Model.BinaryToProtocol( hs.ProtocolID )
+			record.Protocol = Model.BinaryToProtocol(hs.ProtocolID)
 			err = agsmd.UpdateRecord(&record)
 			if err != nil {
 				return nil, err
@@ -130,22 +121,19 @@ func updateProtocol(hs *HSProtocol.HS)(*HSProtocol.HS, error){
 		}
 	}
 
-
 	return &HSProtocol.HS{ // ACK
-		ProtocolID: hs.ProtocolID,
-		Command: 0b0000000000,
-		UUID: hs.UUID,
-		HealthStatus: hs.HealthStatus,
+		ProtocolID:     hs.ProtocolID,
+		Command:        0b0000000000,
+		UUID:           hs.UUID,
+		HealthStatus:   hs.HealthStatus,
 		Identification: hs.Identification,
-		TotalLength: hs.TotalLength,
-		Data: []byte{},
+		TotalLength:    hs.TotalLength,
+		Data:           []byte{},
 	}, nil
 }
 
-
 // Command: 3 (0b0000000011)
-func postSystemInfo (hs *HSProtocol.HS)(*HSProtocol.HS, error){
-
+func postSystemInfo(hs *HSProtocol.HS) (*HSProtocol.HS, error) {
 
 	sysDB := Model.NewSystemInfoDB()
 	Dsys, err := sysDB.Unmarshal(hs.Data)
@@ -159,81 +147,95 @@ func postSystemInfo (hs *HSProtocol.HS)(*HSProtocol.HS, error){
 	}
 
 	return &HSProtocol.HS{ // ACK
-		ProtocolID: hs.ProtocolID,
-		Command: 0b0000000000,
-		UUID: hs.UUID,
-		HealthStatus: hs.HealthStatus,
+		ProtocolID:     hs.ProtocolID,
+		Command:        0b0000000000,
+		UUID:           hs.UUID,
+		HealthStatus:   hs.HealthStatus,
 		Identification: hs.Identification,
-		TotalLength: hs.TotalLength,
-		Data: []byte{},
+		TotalLength:    hs.TotalLength,
+		Data:           []byte{},
 	}, nil
 }
-
 
 // Command: 5 (0b0000000101)
-func postApplicationInfo (hs *HSProtocol.HS)(*HSProtocol.HS, error){
+func postApplicationInfo(hs *HSProtocol.HS) (*HSProtocol.HS, error) {
 
 	appDB := Model.ApplicationDB{}
 	Dapp, err := appDB.Unmarshal(hs.Data)
 	if err != nil {
 		return nil, err
 	}
-	err = appDB.InsertRecord( Dapp )
+	err = appDB.InsertRecord(Dapp)
 	if err != nil {
 		return nil, err
 	}
 
 	return &HSProtocol.HS{ // ACK
-		ProtocolID: hs.ProtocolID,
-		Command: 0b0000000000,
-		UUID: hs.UUID,
-		HealthStatus: hs.HealthStatus,
+		ProtocolID:     hs.ProtocolID,
+		Command:        0b0000000000,
+		UUID:           hs.UUID,
+		HealthStatus:   hs.HealthStatus,
 		Identification: hs.Identification,
-		TotalLength: hs.TotalLength,
-		Data: []byte{},
+		TotalLength:    hs.TotalLength,
+		Data:           []byte{},
 	}, nil
 }
 
-
-
 // Command: 6 (0b0000000110)
-func getProcedure (hs *HSProtocol.HS)(*HSProtocol.HS, error){
+func getProcedure(hs *HSProtocol.HS) (*HSProtocol.HS, error) {
 
 	appDB := Model.ApplicationDB{}
 	Dapp, err := appDB.Unmarshal(hs.Data)
 	if err != nil {
 		return nil, err
 	}
-	err = appDB.InsertRecord( Dapp )
+	err = appDB.InsertRecord(Dapp)
 	if err != nil {
 		return nil, err
 	}
 
 	return &HSProtocol.HS{ // ACK
-		ProtocolID: hs.ProtocolID,
-		Command: 0b0000000000,
-		UUID: hs.UUID,
-		HealthStatus: hs.HealthStatus,
+		ProtocolID:     hs.ProtocolID,
+		Command:        0b0000000000,
+		UUID:           hs.UUID,
+		HealthStatus:   hs.HealthStatus,
 		Identification: hs.Identification,
-		TotalLength: hs.TotalLength,
-		Data: []byte{},
+		TotalLength:    hs.TotalLength,
+		Data:           []byte{},
 	}, nil
 }
 
 // Command: 7 (0b0000000111)
-func postLogOfProcedure (hs *HSProtocol.HS)(*HSProtocol.HS, error){
+func postLogOfProcedure(hs *HSProtocol.HS) (*HSProtocol.HS, error) {
 
-	Model.
+	jbMgr := NewJobManager()
+	var agentUuid string
+	copy(hs.UUID[:], agentUuid)
 
+	job, exist := jbMgr.GetData(agentUuid)
+	if exist == true {
+
+		// technical ID 에 맵핑하여 yaml 파일을 직렬화하고 불러와서
+
+		return &HSProtocol.HS{ // ACK
+			ProtocolID:     hs.ProtocolID,
+			Command:        0b0000000000,
+			UUID:           hs.UUID,
+			HealthStatus:   hs.HealthStatus,
+			Identification: hs.Identification,
+			TotalLength:    hs.TotalLength,
+			Data:           []byte{},
+		}, nil
+	}
+
+	// false
 	return &HSProtocol.HS{ // ACK
-		ProtocolID : hs.ProtocolID,
-		Command: 0b0000000000,
-		UUID: hs.UUID,
-		HealthStatus: hs.HealthStatus,
+		ProtocolID:     hs.ProtocolID,
+		Command:        0b0000000000,
+		UUID:           hs.UUID,
+		HealthStatus:   hs.HealthStatus,
 		Identification: hs.Identification,
-		TotalLength: hs.TotalLength,
-		Data: []byte{},
+		TotalLength:    hs.TotalLength,
+		Data:           []byte{},
 	}, nil
 }
-
-
