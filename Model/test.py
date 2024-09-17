476bffdaa205447f911302f2924d3cfcf9885cab
CherryPichu <uskawjdu@gmail.com>
2024-09-18 04:14:43 +0900

    HTTPS-53 < 커밋삭제한거 복구.... >

---
+++ Model/test.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+# 요청할 URL
+url = 'http://127.0.0.1/postInstruction'
+
+# 전송할 데이터
+data = {
+    "agentUUID": "12342",
+    "procedureID": "P_Collection_Kimsuky_001",
+    "instructionUUID": "f5556669-ffbe-4d24-b833-fc9888fdeaef"
+}
+
+# 헤더 설정
+headers = {
+    'Content-Type': 'application/json'
+}
+
+# POST 요청 전송
+response = requests.post(url, headers=headers, data=json.dumps(data))
+
+# 응답 출력
+if response.status_code == 200:
+    print("Request succeeded:", response.json())
+else:
+    print(f"Request failed with status code {response.status_code}: {response.text}")
